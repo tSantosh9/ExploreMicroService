61ca863bf68e682bdaa1145aa59b04126110658c
tSantosh9 <santosh.93.thapa@gmail.com>
2019-11-12 22:04:28 +0530

    Added a new service for getting the user's history

---
+++ user-history/src/main/java/com/explore/userhistory/UserHistoryApplication.java
@@ -0,0 +1,13 @@
+package com.explore.userhistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserHistoryApplication.class, args);
+	}
+
+}
---
+++ user-history/src/test/java/com/explore/userhistory/UserHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.explore.userhistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
